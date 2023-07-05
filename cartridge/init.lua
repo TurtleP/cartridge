cdca71166feb83ce65b74ead28c8f22e7fb9e9d3
TurtleP <jpostelnek@outlook.com>
2023-07-05 14:52:16 -0400

    initial commit

---
+++ cartridge/init.lua
@@ -0,0 +1,119 @@
+---@class cartridge LÖVE save data module
+---@field private _data table The data we hold
+---@field private _filename string The filename to the save data
+---@field private _config { writeOnFlush: true } Configuration
+
+local cartridge =
+{
+    _VERSION     = "0.1.0",
+    _DESCRIPTION = "LÖVE save data module",
+    _LICENSE     = [[
+       MIT LICENSE
+       Copyright (c) TurtleP
+       Permission is hereby granted, free of charge, to any person obtaining a
+       copy of this software and associated documentation files (the
+       "Software"), to deal in the Software without restriction, including
+       without limitation the rights to use, copy, modify, merge, publish,
+       distribute, sublicense, and/or sell copies of the Software, and to
+       permit persons to whom the Software is furnished to do so, subject to
+       the following conditions:
+       The above copyright notice and this permission notice shall be included
+       in all copies or substantial portions of the Software.
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+       OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+       MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+       IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+       CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+       TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+       SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   ]],
+
+    _data        = nil,
+    _filename    = nil,
+    _config      = { writeOnFlush = true }
+}
+
+local path = (...):gsub("%.init", "")
+local msgpack = require(path .. ".msgpack")
+
+local function export_save()
+    love.filesystem.write(cartridge._filename, msgpack.pack(cartridge._data))
+end
+
+---Creates or loads save data
+---@param name string The name of the save file
+function cartridge.init(name, config)
+    cartridge._filename = name
+    cartridge._config.writeOnFlush = (config and config.writeOnFlush) or true
+
+    if not love.filesystem.getInfo(name, "file") then
+        cartridge._data = {}
+        return cartridge
+    end
+
+    return cartridge.load(name)
+end
+
+function cartridge.load(name)
+    assert(love.filesystem.getInfo(name, "file"), ("file '%s' does not exists."):format(name))
+
+    local contents, size_or_error = love.filesystem.read(name)
+    assert(contents ~= nil, size_or_error)
+
+    cartridge._data = msgpack.unpack(love.filesystem.read(name))
+
+    return cartridge
+end
+
+---Read a key from the save data table
+---@param key string The value from the save data table
+---@return table data The data that was read inside of a table
+function cartridge.read(key)
+    assert(cartridge._data, "cartridge was not loaded with data.")
+    assert(cartridge._data[key], ("key '%s' does not exist in the save data."):format(key))
+
+    local value = cartridge._data[key]
+
+    if type(value) == "table" then
+        return value
+    end
+
+    return { [key] = value }
+end
+
+---Writes a table of data to the save data
+---@param key string The key to write to in the save data
+---@param data table|any The data table to write for the key in the save data
+function cartridge.write(key, data)
+    assert(key, ("bad argument #1 for 'key': expected string, got %s"):format(type(key)))
+
+    if type(data) == "string" then
+        return cartridge.write_value(key, data)
+    end
+
+    assert(data, ("bad argument #2 for 'data': expected table, got %s"):format(type(data)))
+
+    cartridge._data[key] = data
+
+    if cartridge._config.writeOnFlush then
+        export_save()
+    end
+end
+
+---Write a single value of data to the save data
+---@param key string key to write to in the save data
+---@param value any The data value to write for the key in the save data
+function cartridge.write_value(key, value)
+    assert(key and type(key) == "string", ("bad argument #1 for 'key': expected string, got %s"):format(type(key)))
+    assert(value, "bad argument #2 for 'data': expected non-nil")
+
+    cartridge._data[key] = value
+
+    if cartridge._config.writeOnFlush then
+        export_save()
+    end
+end
+
+cartridge.save_all = export_save
+
+return cartridge
